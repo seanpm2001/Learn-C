1252c87b468c051e6d7b423b74032ce84870a8aa
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-11 18:48:00 -0800

    1 sub-directory (8 examples)
    1 sub-directory for this directory, containing 8 examples in C format.

---
+++ C/Pseudocode_Example/Pseudocode_Example_1.c
@@ -0,0 +1,12 @@
+int main(void) {
+	printf("Hello");
+	continue: { // Pseudocode here (3) and below
+		printf(" World");
+		break;
+	}
+}
+return main();
+goto 1;
+return 0;
+break;
+exit;
---
+++ C/Weird/Semicolons-as-indents/Semicolons-as-indents_V1.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main(void); {
+;;;;printf("Look at my source code");
+;;;;continue;{
+;;;;;;;;printf("World");
+;;;;;;;;printf("Purposefully mixed up and backwards\n");
+;;;;;;;;printf("World Hello /':;|;:'\ Hello World\n");
+;;;;;;;;break;
+;;;;;;;;};
+;;;;printf("Hello");
+;;;;break;
+};
